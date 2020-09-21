4ba74cf70d80ef0fd68de72badbe7349f5f6e618
ammirabile <iammirabile80@gmail.com>
2020-09-21 18:36:14 +1000

    Version 2 - Working but not complete

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ class cell {
 }
 
 //Create MineField
-function createMineField(field){
+function createMineField(){
     let thisField = []; 
     for(let i = 0; i < 10 ; i++){
         thisField[i] = [];
@@ -33,11 +33,16 @@ function printField(field){
     for (let i = 0; i < field.length ; i++){
         let row = i + " ";
         for(let j = 0; j <field[i].length; j++){
-            if(field[i][j].isBomb === true){
-                row += "B " ;
+            if(field[i][j].isOpen === true){
+                if(field[i][j].isBomb === true){
+                    row += "B " ;
+                }
+                else {
+                    row += field[i][j].bombsClose + " ";
+                }    
             }
             else {
-                row += "  "
+                row += "  " ;
             }
         }
         console.log(row)
@@ -46,19 +51,31 @@ function printField(field){
 }
 
 //Place Bombs
-function placeBombs(arr){
-    for(let i = 0; i < 5 ; i++){
-        let coord1 = Math.floor(Math.random() * arr.length);
-        let coord2 = Math.floor(Math.random() * arr.length);
-        mineField[coord1][coord2].isBomb = true;
-        console.log(mineField[coord1][coord2]);
-        console.log(coord1,coord2);       
-        bombList.push([coord1,coord2]);
+function placeBombs(field){
+
+    for(let i = 0; i < 10 ; i++){
+        let bombPlaced = false;
+        while(!bombPlaced){
+            let coord1 = Math.floor(Math.random() * field.length);
+            let coord2 = Math.floor(Math.random() * field.length);
+            if(field[coord1][coord2].isBomb === true){
+                bombPlaced = false;
+            }
+            else {
+                field[coord1][coord2].isBomb = true;
+                bombPlaced = true;
+            }
+            console.log(field[coord1][coord2]);
+            console.log(coord1,coord2);      
+            bombList.push([coord1,coord2]);    
+        }
+        
     }
 }
 
-function isValidCoord(coord1,coord2){
-    if(coord1 < 0 || coord2 < 0){
+function isValidCoord(coord1,coord2,arr){
+    console.log("Valid Coord Check :" + arr.length);
+    if(coord1 < 0 || coord2 < 0 || coord1 >= arr.length || coord2 >= arr.length){
         return false;
     }
     return true;
@@ -72,20 +89,68 @@ function lookForBombs(){
         let coord2 = coord[1];
         for(let rowCheck of checkCoord){
             for(let colCheck of checkCoord){
-                if(isValidCoord([coord[0]+rowCheck],[coord[1]+colCheck])){
-                    console.log(mineField[1][1]);
-                    console.log(mineField[coord[0]+rowCheck][coord[1]+colCheck]);
-                    mineField[coord[0]+rowCheck][coord[1]+colCheck].bombsClose += 1;
-                    console.log(mineField[coord[0]+rowCheck][coord[1]+colCheck]);
+                let x = coord1 + rowCheck;
+                let y = coord2 + colCheck;
+                // console.log(coord);
+                // console.log(x + " " + y);
+                // console.log(typeof(colCheck));
+                // console.log(isValidCoord(x,y,mineField));
+                if(isValidCoord(x,y,mineField)){
+                    // console.log(mineField[x][y]);
+                    mineField[x][y].bombsClose += 1;
                 }
             }
         }
     } 
 }
 
-mineField = createMineField(mineField);
+function checkPlay(coord, grid) {
+    if (coord.length !== 3 || coord.indexOf(",") === -1) {
+        return false;
+    } else if (coord[0] > grid.length || coord[2] > grid.length) {
+        return false;
+    } else if (grid[coord[0]][coord[2]].isOpen === true){
+        return false;
+    }
+    return true;
+
+}
+function openCell(grid,coord){
+    grid[coord[0]][coord[2]].isOpen = true;
+    return grid[coord[0]][coord[2]].isBomb;
+}
+
+mineField = createMineField();
 printField(mineField);
 placeBombs(mineField);
 printField(mineField);
 lookForBombs();
+printField(mineField);
+
+let totalBombs = 10;
+let lifes = 3;
+let gameOver = false;
+while (!gameOver){
+    let validPlay = false;
+    let play ;
+    while(!validPlay){
+        play = prompt("Choose a coordenate separated by comma");
+        if (checkPlay(play,mineField)){
+            validPlay = checkPlay(play,mineField);
+        }
+        else {console.log(`Not a valid coordenate!`);
+
+        }
+    }
+    console.log(`Valid Play - ${play}`);
+    if (openCell(mineField,play) === true){
+        lifes -= 1;
+        console.log(`BOOOOOOOM you lost 1 life - ${lifes} left`);
+
+    }
+    printField(mineField);
+}
+
+    
+
 
