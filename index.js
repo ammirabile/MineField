4166261aad2956d3bf49d2dcecc964877ab2d64e
ammirabile <iammirabile80@gmail.com>
2020-10-04 09:53:26 +1100

    new version

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function printBoard(board) {
     for (let i = 0; i < board.length; i++) {
         let row = i + " ";
         for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j].isOpen === false) {
+            if (board[i][j].isOpen === true) {
                 //change here
                 if (board[i][j].isBomb === true) {
                     row += "B ";
@@ -91,33 +91,39 @@ function openCell(coord1, coord2) {
 }
 
 //GetNeighbours
-function getNeighbours(coord1, coord2, board) {
+function getNeighbours(board) {
     let checkCoord = [-1, 0, 1];
-    let arr = [];
     let newCoord = "";
-    for (let i = 0; i < checkCoord.length; i++) {
-        for (let j = 0; j < checkCoord.length; j++) {
-            let x;
-            let y;
-            x = coord1 + checkCoord[i];
-            y = coord2 + checkCoord[j];
-            newCoord = `${x},${y}`;
-            if (
-                newCoord !== coord1 + "," + coord2 &&
-                isValidCoord(x, y, board)
-            ) {
-                arr.push(newCoord);
+    for (let coord1 = 0; coord1 < board.length; coord1++) {
+        for (let coord2 = 0; coord2 < board[coord1].length; coord2++) {
+            for (let i = 0; i < checkCoord.length; i++) {
+                for (let j = 0; j < checkCoord.length; j++) {
+                    let x = 0;
+                    let y = 0;
+                    x = parseInt(coord1) + checkCoord[i];
+                    y = parseInt(coord2) + checkCoord[j];
+                    console.log(x);
+                    console.log(y);
+                    newCoord = `${x},${y}`;
+                    if (
+                        newCoord !== coord1 + "," + coord2 &&
+                        isValidCoord(x, y, board)
+                    ) {
+                        board[coord1][coord2].neighbours.push(newCoord);
+                    }
+                }
             }
         }
     }
-    return arr;
 }
 
 function checkForNearBombs(bList) {
     for (let i = 0; i < bList.length; i++) {
         let x = bList[i][0];
         let y = bList[i][1];
-        let neighbours = getNeighbours(x, y, board);
+        console.log(x);
+        console.log(y);
+        let neighbours = board[x][y].neighbours;
         console.log(neighbours);
         for (coord of neighbours) {
             if (!board[coord[0]][coord[2]].isBomb) {
@@ -128,5 +134,42 @@ function checkForNearBombs(bList) {
 }
 
 board = createBoard();
+getNeighbours(board);
 placeBombs(board);
-printBoard(board);
+
+let gameOver = false;
+let lives = 3;
+let playerCoord1;
+let playerCoord2;
+
+while (!gameOver) {
+    console.log(`***** You have ${lives} lives left ******`);
+    printBoard(board);
+    let validPlay = false;
+    while (!validPlay) {
+        var play = prompt("Choose your coordenates");
+        playerCoord1 = play[0];
+        playerCoord2 = play[2];
+        validPlay = isValidCoord(playerCoord1, playerCoord2, board);
+        if (!validPlay) console.log("Not a valid coordenate!");
+    }
+    console.log(`You played - ${play}`);
+    openCell(playerCoord1, playerCoord2);
+    if (board[playerCoord1][playerCoord2].isBomb) {
+        console.log("*******BOOOOMM you choose a bomb*******");
+        lives--;
+        console.log(`Lives left ${lives}`);
+    } else if (board[playerCoord1][playerCoord2].bombsClose === 0) {
+        let neighbours = board[playerCoord1][playerCoord2].neighbours;
+        for (let i = 0; i < neighbours.length; i++) {
+            openCell(neighbours[i][0], neighbours[i][2]);
+            //more logic here
+        }
+    } else {
+        openCell(playerCoord1, playerCoord2);
+    }
+    if (lives === 0) {
+        console.log(`Game Over`);
+        gameOver = true;
+    }
+}
